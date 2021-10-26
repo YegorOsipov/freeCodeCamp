3a82054918d4dd65ba060318060c76c23fafe72f
Egor <gogsyan11@gmail.com>
2021-10-26 20:41:21 +0300

    циклы

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -59,3 +59,86 @@ var myStorage = {
 };
 
 var gloveBoxContents = myStorage.car.inside['glove box'];
+
+
+// Accessing Nested Arrays - доступ к вложенным массивам
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+var secondTree = myPlants[1].list[1];
+
+
+// Record Collection
+var recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+function updateRecords(records, id, prop, value) {
+  if (prop != 'tracks' && value != '') {
+
+  }
+}
+
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
+
+
+// Iterate with JavaScript While Loops - цикл while
+var myArray = [];
+var i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+
+// Iterate with JavaScript For Loops - цикл for
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (let i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
+
+
+// Iterate Odd Numbers With a For Loop
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (let i = 0; i < 10; i += 1) {
+  myArray.push(i);
+}
+console.log(myArray);
