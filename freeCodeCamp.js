2d68e846e6fbe44b6ddacab9f7d1ce3f0b7318a5
Egor <gogsyan11@gmail.com>
2021-10-26 11:51:48 +0300

    object1

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,10 +1,61 @@
 function checkObj(obj, checkProp) {
-    // Only change code below this line
-     if (obj.hasOwnProperty(checkProp)) {
-       return checkProp;
-     } else {
-       return 'Not Found';
-     }
-    
-    // Only change code above this line
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return 'Not Found';
   }
+}
+
+let obj = {
+  1: 'eee',
+  2: 'rrr',
+  3: 'ttt'
+};
+console.log(checkObj(obj, 1));
+
+
+// Manipulating Complex Objects
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+var album = {
+  "artist": "Daft Punk",
+  "title": "Homework",
+  "release_year": 1997,
+  "formats": [ 
+    "CD",
+    "Cassette",
+    "LP"
+    ],
+   "gold": true 
+}
+
+myMusic.push(album);
+console.log(myMusic);
+
+
+// Accessing Nested Objects
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside['glove box'];
