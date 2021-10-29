472d73eefb1cd53178392c9ccbaada48472c7ed7
Egor <gogsyan11@gmail.com>
2021-10-29 14:43:42 +0300

    ES6 Деструктуризация объектов и назначение переменных

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -428,6 +428,17 @@ const { today, tomorrow} = HIGH_TEMPERATURES;
 // Only change code above this line
 
 
+// Use Destructuring Assignment to Assign Variables from Objects
+const HIGH_TEMPERATURE = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+// Only change code below this line 
+const { today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURE; 
+// Only change code above this line
+
+
 
 
 
