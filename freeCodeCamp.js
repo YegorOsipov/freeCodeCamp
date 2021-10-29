e8350867ff955b3974add05ff619c7180bcabf03
Egor <gogsyan11@gmail.com>
2021-10-29 13:35:59 +0300

    ES6 arrow func

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -348,6 +348,38 @@ function checkScope() {
 
 
 // Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // Only change code above this line
+}
+editInPlace();
+
+
+// Prevent Object Mutation
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+
+// Use Arrow Functions to Write Concise Anonymous Functions
+const magic = () => new Date();
 
 
 
