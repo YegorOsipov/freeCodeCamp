3c4e7faab3789901820a2d49aa17beb36e548323
Egor <gogsyan11@gmail.com>
2021-10-29 14:27:31 +0300

    ES6 spread

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -406,5 +406,18 @@ const sum = (...args) => {
 }
 
 
+// Use the Spread Operator to Evaluate Arrays In-Place
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+console.log(arr2);
+
+
+
+
+
+
+
 
 
