54f8c5b19bb6b4f1a8aff8cde7d5d9aae9ba8542
Egor <gogsyan11@gmail.com>
2021-10-29 13:47:30 +0300

    ES6 arrow func default par

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -383,10 +383,15 @@ const magic = () => new Date();
 
 
 // Write Arrow Functions with Parameters
-var myConcat = (arr1, arr2) => arr1.concat(arr2);
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 console.log(myConcat([1, 2], [3, 4, 5]));
 
+
+// Set Default Parameters for Your Functions
+// Only change code below this line
+const increment = (number, value) => number + value;
+// Only change code above this line
 
 
 
