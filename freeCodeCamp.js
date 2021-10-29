11d1706825f1359d27f127d012398fe7db487831
Egor <gogsyan11@gmail.com>
2021-10-29 13:37:57 +0300

    ES6 arrow func with parametrs

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -382,6 +382,12 @@ const PI = freezeObj();
 const magic = () => new Date();
 
 
+// Write Arrow Functions with Parameters
+var myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+
 
 
 
