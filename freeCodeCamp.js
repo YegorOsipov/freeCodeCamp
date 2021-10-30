a3b6de0744db960ee79be907ede5bcb9404ff853
Egor <gogsyan11@gmail.com>
2021-10-30 23:29:06 +0300

    ES6 шаблон LiteralsPassed

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -483,5 +483,23 @@ const half = ({max, min}) => (max + min) / 2.0;
 // Only change code above this line
 
 
+// Create Strings using Template Literals
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${result.failure[i]}</li>`)
+  }
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
 
 
